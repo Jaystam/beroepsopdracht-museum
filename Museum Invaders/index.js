26c461d5562657a344ccf481b13777a659f45835
Luuk <35917@ma-web.nl>
2023-01-19 16:59:05 +0100

    Player movement

--- Museum Invaders/index.js
+++ Museum Invaders/index.js
@@ -13,6 +13,8 @@ class Player {
             y: 0
         }
 
+        this.rotation = 0
+
         const image = new Image()
         image.src = './img/spaceship.png'
         image.onload = () => {
@@ -20,31 +22,156 @@ class Player {
             this.image = image
             this.width = image.width * scale
             this.height = image.height * scale
+            this.position = {
+                x: canvas.width / 2 - this.width / 2,
+                y: canvas.height - this.height - 20
+            }
         }
-
-
     }
 
     draw() {
         //c.fillStyle = 'red'
         //c.fillRect(this.position.x, this.position.y, this.width, this.height)
-        if (this.image)
-            c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
-        this.position = {
-            x: canvas.width / 2 - this.width / 2,
-            y: canvas.height - this.height - 20
+
+        c.save()
+        c.translate(
+            player.position.x + player.width / 2,
+            player.position.y + player.height / 2
+        )
+        c.rotate(this.rotation)
+
+        c.translate(
+            -player.position.x - player.width / 2,
+            -player.position.y - player.height / 2
+        )
+
+        c.drawImage(this.image,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        )
+        c.restore()
+
+    }
+
+    update() {
+        if (this.image) {
+            this.draw()
+            this.position.x += this.velocity.x
         }
     }
 }
 
+class Projectile {
+    constructor({ position, velocity }) {
+        this.position = position
+        this.velocity = velocity
+
+        this.radius = 3
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = 'red'
+        c.fill()
+        c.closePath()
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
+
 const player = new Player()
-player.draw()
+const projectiles = []
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    space: {
+        pressed: false
+    }
+
+}
 
 function animate() {
     requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
-    player.draw()
+    player.update()
+    projectiles.forEach((Projectile, index) => {
+
+        if (Projectile.position.y + Projectile.radius <= 0) {
+            setTimeout(() =>{
+                projectiles.splice(index, 1)
+            }, 0)
+        } else {
+            Projectile.update()
+        }
+    })
+
+    if (keys.a.pressed && player.position.x >= 0) {
+        player.velocity.x = -7
+        player.rotation = -0.15
+    } else if (keys.d.pressed && player.position.x + player.width <= canvas.width) {
+        player.velocity.x = 7
+        player.rotation = 0.15
+    } else {
+        player.velocity.x = 0
+        player.rotation = 0
+    }
 }
 
 animate()
+
+addEventListener('keydown', ({ key }) => {
+    switch (key) {
+        case 'a':
+            //console.log('left')
+            keys.a.pressed = true
+            break
+        case 'd':
+            //console.log('right')
+            keys.d.pressed = true
+            break
+        case ' ':
+            //console.log('space')
+            projectiles.push(new Projectile({
+                position: {
+                    x: player.position.x + player.width / 2,
+                    y: player.position.y
+                },
+                velocity: {
+                    x: 0,
+                    y: -10
+                }
+            }))
+
+            //console.log(projectiles)
+            break
+    }
+})
+
+addEventListener('keyup', ({ key }) => {
+    switch (key) {
+        case 'a':
+            //console.log('left')
+            keys.a.pressed = false
+            break
+        case 'd':
+            //console.log('right')
+            keys.d.pressed = false
+            break
+        case ' ':
+            //console.log('space')
+            break
+    }
+})
