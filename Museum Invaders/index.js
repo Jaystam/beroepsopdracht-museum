378e4eb1b321ff7eaefd290e0a465d85e996b278
Luuk <35917@ma-web.nl>
2023-01-21 17:47:16 +0100

    Invaders grids

--- Museum Invaders/index.js
+++ Museum Invaders/index.js
@@ -87,7 +87,7 @@ class Projectile {
 }
 
 class Invader {
-    constructor() {
+    constructor({ position }) {
 
 
         this.velocity = {
@@ -104,8 +104,8 @@ class Invader {
             this.width = image.width * scale
             this.height = image.height * scale
             this.position = {
-                x: canvas.width / 2 - this.width / 2,
-                y: canvas.height / 2
+                x: position.x,
+                y: position.y
             }
         }
     }
@@ -122,12 +122,57 @@ class Invader {
         )
     }
 
-    update() {
+    update({ velocity }) {
         if (this.image) {
             this.draw()
-            this.position.x += this.velocity.x
-            this.position.y += this.velocity.y
-            
+            this.position.x += velocity.x
+            this.position.y += velocity.y
+
+        }
+    }
+}
+
+class Grid {
+    constructor() {
+        this.position = {
+            x: 0,
+            y: 0
+        }
+
+        this.velocity = {
+            x: 3,
+            y: 0
+        }
+
+        this.invaders = []
+        const columns = Math.floor(Math.random() * 10 + 5)
+        const rows = Math.floor(Math.random() * 5 + 2)
+
+        this.width = columns * 30
+
+        for (let x = 0; x < columns; x++) {
+            for (let y = 0; y < rows; y++) {
+                this.invaders.push(new Invader({
+                    position: {
+                        x: x * 30,
+                        y: y * 30
+                    }
+                }))
+            }
+        }
+        console.log(this.invaders)
+    }
+
+    update() {
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        this.velocity.y = 0
+
+        if (this.position.x + this.width >= canvas.width ||
+            this.position.x <= 0) {
+            this.velocity.x = -this.velocity.x
+            this.velocity.y = 30
         }
     }
 }
@@ -135,7 +180,7 @@ class Invader {
 
 const player = new Player()
 const projectiles = []
-const invader = new Invader()
+const grids = []
 const keys = {
     a: {
         pressed: false
@@ -149,21 +194,32 @@ const keys = {
 
 }
 
+let frames = 0
+let randomInterval = Math.floor(Math.random() * 500 + 500)
+
+console.log(randomInterval)
+
 function animate() {
     requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
-    invader.update()
     player.update()
     projectiles.forEach((Projectile, index) => {
 
         if (Projectile.position.y + Projectile.radius <= 0) {
-            setTimeout(() =>{
+            setTimeout(() => {
                 projectiles.splice(index, 1)
             }, 0)
         } else {
             Projectile.update()
         }
+    })
+
+    grids.forEach(grid => {
+        grid.update()
+        grid.invaders.forEach(invader => {
+            invader.update({ velocity: grid.velocity })
+        })
     })
 
     if (keys.a.pressed && player.position.x >= 0) {
@@ -176,6 +232,16 @@ function animate() {
         player.velocity.x = 0
         player.rotation = 0
     }
+
+    console.log(frames)
+    //spawning enemies
+    if (frames % randomInterval === 0) {
+        grids.push(new Grid())
+        randomInterval = Math.floor(Math.random() * 500 + 500)
+        frames = 0
+        console.log(randomInterval)
+    }
+    frames++
 }
 
 animate()
